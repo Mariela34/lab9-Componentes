c58d3fed223c0b0d274b2724147077530589e70a
Mariela  Bonilla <mbonilla.guti@gmail.com>
2020-11-28 02:38:59 -0600

    Primer commmit

---
+++ SeguridadSpring/src/main/java/com/cenfotec/seguridad/config/MemorySecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.cenfotec.seguridad.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class MemorySecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		String encodedPassword = new BCryptPasswordEncoder().encode("mariela");
+		auth.inMemoryAuthentication()
+		.withUser("mariela")
+		.password(encodedPassword)
+		.roles("manager", "dev")
+		.and().passwordEncoder(new BCryptPasswordEncoder());
+	}
+}
